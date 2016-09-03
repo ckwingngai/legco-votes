44cb168527ad798c2ed94e75de2d04084d5a8eee
raymond <ckwingngai@gmail.com>
2016-09-03 12:36:24 +0800

    import all

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,22 @@
-$arr = [
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160713.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160706.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160622.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160615.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160608.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160601.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160525.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160518.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160511.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160420.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160316.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160217.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160127.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160120.xml',
-  'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160106.xml',
+$arr = {
+  "six": [
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160713.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160706.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160622.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160615.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160608.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160601.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160525.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160518.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160511.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160420.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160316.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160217.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160127.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160120.xml',
+    'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20160106.xml'
+  ],
+  "five": [
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151216.xml',
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151202.xml',
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151125.xml',
@@ -22,7 +25,93 @@ $arr = [
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151104.xml',
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151028.xml',
   'http://www.legco.gov.hk/yr15-16/chinese/counmtg/voting/cm_vote_20151014.xml',
-]
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150708.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150624.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150617.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150610.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150603.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150527.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150520.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150422.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150325.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150318.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150211.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150204.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150128.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150121.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20150107.xml'
+  ],
+  "four": [
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141217.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141210.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141203.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141126.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141120.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141112.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141105.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141029.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141022.xml',
+  'http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/cm_vote_20141015.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140709.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140625.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140618.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140611.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140604.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140528.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140521.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140507.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140416.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140326.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140319.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140219.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140212.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140122.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20140108.xml'
+  ],
+  "three": [
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131218.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131211.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131204.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131127.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131120.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131113.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131106.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131030.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131023.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131016.xml',
+  'http://www.legco.gov.hk/yr13-14/chinese/counmtg/voting/cm_vote_20131009.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130717.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130710.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130703.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130626.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130619.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130605.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130529.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130522.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130515.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130508.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130424.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130327.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130320.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130220.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130206.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130130.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130123.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20130109.xml'
+  ],
+  "two": [
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121219.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121212.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121205.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121128.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121121.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121114.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121107.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121031.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121024.xml',
+  'http://www.legco.gov.hk/yr12-13/chinese/counmtg/voting/cm_vote_20121017.xml'
+  ]
+}
 
 require 'net/http'
 
@@ -46,7 +135,6 @@ class VotesController < ApplicationController
 
   def import_item
     url = "http://www.legco.gov.hk/yr14-15/chinese/counmtg/voting/" + params[:date] + ".xml"
-    puts url 
     handle_import_item(url, params[:pos])
   end
 
@@ -87,8 +175,7 @@ class VotesController < ApplicationController
   end
 
   def import
-
-    $arr.each do |item|
+    $arr[params[:year].to_sym].each do |item|
       url = URI.parse(item)
       req = Net::HTTP::Get.new(url.to_s)
       res = Net::HTTP.start(url.host, url.port) {|http|
@@ -105,7 +192,7 @@ class VotesController < ApplicationController
         end
       else
         puts "one"
-        insert_db(votes)
+        insert_db(votes["vote"])
       end
     end
 
@@ -115,9 +202,7 @@ class VotesController < ApplicationController
     @result = []
     @data = VoteHist.all
     @data.each do |item|
-      puts item.id
       hash = get_item(item.id)
-      puts hash
       @result.push(hash)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'votes#list'
-  get 'import' => 'votes#import'
+  get 'import/:year' => 'votes#import'
   get 'import_item/:date/(:pos)' => 'votes#import_item'
   get 'api_get_item/:id' => 'votes#api_get_item'
   get 'destroy/:id' => 'votes#destroy'
